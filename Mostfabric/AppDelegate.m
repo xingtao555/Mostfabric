d5745ac70bb8b6c5a8a930e15c3fa0df26f2d341
taoxing <taoxing@taoxingdeMacBook-Pro.local>
2017-07-27 19:04:38 +0800

    test

--- Mostfabric/AppDelegate.m
+++ Mostfabric/AppDelegate.m
@@ -24,7 +24,7 @@
     YTKNetworkConfig *config = [YTKNetworkConfig sharedInstance];
     config.baseUrl = kBaseUrl;
     NSLog(@"hahah");
-    NSLog(@"branch20170727test111");
+    NSLog(@"branch20170727test222");
     [self.window makeKeyAndVisible];
     
     
